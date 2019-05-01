987ce23cff79e05f412d559d4b1c928472a1481c
otofune <otofune@otofune.me>
2019-05-02 08:31:22 +0900

    feat(timeline): Show more information

--- src/presenters/Post.tsx
+++ src/presenters/Post.tsx
@@ -2,4 +2,10 @@ import * as React from 'react'
 
 import { Post } from '../models'
 
-export default ({ post }: { post: Post }) => (<div>@{post.author.screenName} {post.text}</div>)
+export default ({ post }: { post: Post }) => (
+  <div>
+    <b>{post.author.name}</b> @{post.author.screenName} [{post.application.name}] {post.createdAt.toString()}
+    <br />
+    {post.text}
+  </div>
+)
