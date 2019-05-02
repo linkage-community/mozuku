9b64ce250156bc8d4824a8f305a135b064d5a819
P-Chan <hello@0x50.io>
2019-05-02 13:12:17 +0900

    feat(*): Add temporary styles

--- src/presenters/Post.tsx
+++ src/presenters/Post.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react'
+import moment from 'moment-timezone'
 
 import { Post } from '../models'
 
 export default ({ post }: { post: Post }) => (
-  <div>
-    <b>{post.author.name}</b> @{post.author.screenName} [{post.application.name}] {post.createdAt.toString()}
-    <br />
-    {post.text}
+  <div className="post">
+    <div className="post__head post-head">
+      <div className="post-head__name">
+        <span className="post-head__name__name">{post.author.name}</span>
+        <span className="post-head__name__screenName">@{post.author.screenName}</span>
+      </div>
+      <div className="post-head__time">
+        {moment(post.createdAt.toString()).tz('Asia/Tokyo').format('HH:mm:ss · D MMM YYYY')}
+      </div>
+    </div>
+    <div className="post__body">
+      {post.text}
+    </div>
+    <div className="post__meta">
+      This post from {post.application.name}
+    </div>
   </div>
 )
