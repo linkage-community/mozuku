86452055b22eded2952bb57a74b7ddf502e05192
otofune <otofune@otofune.me>
2019-06-12 15:57:41 +0000

    chore(license): Correct short license in src/index.tsx (#121)

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 /*
- * mozuku - One of seaweed, client of rinsuki/sea
- * Copyright (C) 2019 otofune <otofune@otofune.me>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  Mozuku - one of seaweed, a web based client for rinsuki/sea.
+  Copyright (C) 2019 otofune <otofune@otofune.me>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import * as React from 'react'
