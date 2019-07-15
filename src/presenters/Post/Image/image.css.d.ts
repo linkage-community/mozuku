83d915f223201522578993b3862867947c8e318a
96a86c <96a86c@googlegroups.com>
2019-07-15 20:04:36 +0900

    add image-size text at lower right of an image

--- src/presenters/Post/Image/image.css.d.ts
+++ src/presenters/Post/Image/image.css.d.ts
@@ -1,4 +1,6 @@
 declare const styles: {
   readonly image: string
+  readonly image__size: string
+  readonly imageCount: string
 }
 export = styles
--- src/presenters/Post/Image/index.tsx
+++ src/presenters/Post/Image/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
+import filesize from 'filesize'
 import AlbumFile from '../../../models/AlbumFile'
-import { PostImage } from '../../../models/Post'
 
 import * as styles from './image.css'
 
@@ -15,30 +15,39 @@ export default ({
     const imageFiles = files.filter(f => f.type === 'image')
     if (imageFiles.length === 0) return <></>
     return (
-      <div className={styles.image}>
-        {imageFiles.length ? (
-          imageFiles.map((im, k) => (
-            <picture key={k}>
-              {im.thumbnails.map(t => (
-                <source key={t.id} srcSet={t.url.href} type={t.mime} />
-              ))}
-              <img
-                title={im.fileName}
-                onClick={() => {
-                  history.pushState(
-                    history.state,
-                    im.fileName,
-                    `#image_${im.id}`
-                  )
-                  setModalContent(im)
-                }}
-              />
-            </picture>
-          ))
-        ) : (
-          <></>
-        )}
-      </div>
+      <>
+        <div className={styles.imageCount}>{imageFiles.length} attachments</div>
+
+        <div className={styles.image}>
+          {imageFiles.length ? (
+            imageFiles.map((im, k) => (
+              <>
+                <picture key={k}>
+                  {im.thumbnails.map(t => (
+                    <source key={t.id} srcSet={t.url.href} type={t.mime} />
+                  ))}
+                  <img
+                    title={im.fileName}
+                    onClick={() => {
+                      history.pushState(
+                        history.state,
+                        im.fileName,
+                        `#image_${im.id}`
+                      )
+                      setModalContent(im)
+                    }}
+                  />
+                </picture>
+                <div className={styles.image__size}>
+                  <p>{filesize(im.direct.size)}</p>
+                </div>
+              </>
+            ))
+          ) : (
+            <></>
+          )}
+        </div>
+      </>
     )
   }, [files.length])
 }
