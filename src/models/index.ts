c20560799b64aa3338b56ddc0f365def79ca9131
otofune <otofune@otofune.me>
2019-09-07 19:20:10 +0900

    :package: Use @linkage-community/bottlemail instead of PostBody (#234)
    * :package: Use @linkage-community/bottlemail instead of PostBody
    
    * Fix notification with nodes
    
    * :lock:
    
    * Remove PostBody code from appStore
    
    * Fix Notification ;;
    
    * trim body for notification
    
    * Disable jest during there are no tests

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,3 @@
 export { default as Account } from './Account'
 export { default as Application } from './Application'
-export {
-  default as Post,
-  NewBoldMyScreenNameMiddleware,
-  pruneEmptyTextMiddleware,
-  BODYPART_TYPE_TEXT,
-  BODYPART_TYPE_BOLD,
-  BODYPART_TYPE_LINK
-} from './Post'
+export { default as Post } from './Post'
