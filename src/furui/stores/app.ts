de2b3320299c338d69c93739ebe537a4c8a7c7c9
otofune <otofune@otofune.me>
2020-02-09 03:31:01 +0900

    :recycle: Remove unnessesary code

--- src/furui/stores/app.ts
+++ src/furui/stores/app.ts
@@ -78,11 +78,6 @@ class SApp {
     window.addEventListener('visibilitychange', () => {
       this.setHidden(document.hidden)
     })
-    window.document.addEventListener('keypress', ev => {
-      if (this.shortcuts.has(ev.charCode)) {
-        this.shortcuts.get(ev.charCode)!(ev)
-      }
-    })
 
     if (this.getPreference(PREFERENCE_FORCE_DARK_THEME)) {
       this.enableForceDarkTheme()
