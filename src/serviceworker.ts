949e9d39a25c214ff9301dc91935de9f8d4aec32
rinsuki <428rinsuki+git@gmail.com>
2020-02-09 01:51:25 +0900

    Use ogp-syutoku-kun.herokuapp.com instead of anallizatore.prezzemolo.org (#418)
    Replace w/ service worker

--- src/serviceworker.ts
+++ src/serviceworker.ts
@@ -2,4 +2,11 @@ self.addEventListener('activate', () => {
   console.log('ServiceWorker is activated.')
 })
 
-self.addEventListener('fetch', () => {})
+self.addEventListener('fetch', (event: unknown) => {
+  const fetchEvent = event as FetchEvent
+  const url = new URL(fetchEvent.request.url)
+  if (url.hostname !== 'analizzatore.prezzemolo.org') return
+  url.host = 'ogp-syutoku-kun.herokuapp.com'
+  url.pathname = '/api/v1/fetch'
+  fetchEvent.respondWith(fetch(url.href))
+})
