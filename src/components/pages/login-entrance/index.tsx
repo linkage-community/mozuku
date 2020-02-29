3293cf11ffc205581f5eab4ebfe345c3815b8ae9
Kohei Eguchi <otofune@otofune.me>
2020-03-01 01:33:56 +0900

    Remove sign-in warning message from login-entrance (#486)

--- src/components/pages/login-entrance/index.tsx
+++ src/components/pages/login-entrance/index.tsx
@@ -17,13 +17,6 @@ export default ({ authURL }: { authURL: URL }) => (
     <BodyContainer>
       <div className={styles.landing}>
         <h1>Greetings</h1>
-        {/* FIXME: このメッセージは仮置き */}
-        現時点では Mozuku を認証する前に
-        <b>あらかじめSeaがログイン状態である必要があります</b>。<br />
-        ->{' '}
-        <a href={authURL.origin} target="_blank">
-          ログインする
-        </a>
         <h2>
           <a href={authURL.href}>認証する</a>
         </h2>
