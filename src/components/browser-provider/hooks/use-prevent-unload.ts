0b0f563108fc4809580b0d3796e0b242041fb205
otofune <otofune@otofune.me>
2020-03-01 01:10:30 +0900

    Add dummy returnValue

--- src/components/browser-provider/hooks/use-prevent-unload.ts
+++ src/components/browser-provider/hooks/use-prevent-unload.ts
@@ -5,7 +5,10 @@ export const usePreventUnload = (handler: PreventHandler) => {
   useEffect(() => {
     const handlerWrapper = (event: BeforeUnloadEvent) => {
       const shouldPreventUnload = handler()
-      if (shouldPreventUnload) event.preventDefault()
+      if (shouldPreventUnload) {
+        event.preventDefault()
+        event.returnValue = 'dummy'
+      }
     }
     window.addEventListener('beforeunload', handlerWrapper)
     return () => {
