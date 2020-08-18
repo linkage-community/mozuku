4293ddc793de8c479f087c514eaa3b4a15b5cb53
Tosuke <tasukeprg@gmail.com>
2020-08-18 21:32:13 +0900

    feat: stop hijacking requests

--- src/serviceworker.ts
+++ src/serviceworker.ts
@@ -1,12 +1,3 @@
 self.addEventListener('activate', () => {
   console.log('ServiceWorker is activated.')
 })
-
-self.addEventListener('fetch', (event: any) => {
-  const fetchEvent = event
-  const url = new URL(fetchEvent.request.url)
-  if (url.hostname !== 'analizzatore.prezzemolo.org') return
-  url.host = 'ogp-syutoku-kun.herokuapp.com'
-  url.pathname = '/api/v1/fetch'
-  fetchEvent.respondWith(fetch(url.href))
-})
