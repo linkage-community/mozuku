eae6fc156e2ef63ecca5264b735b735bd5df1ef8
Tosuke <tasukeprg@gmail.com>
2020-08-18 22:22:07 +0900

    feat: enable navigation preload

--- src/serviceworker.ts
+++ src/serviceworker.ts
@@ -1,3 +1,12 @@
-self.addEventListener('activate', () => {
+const ctx: ServiceWorkerGlobalScope = self as any
+ctx.addEventListener('activate', (ev) => {
   console.log('ServiceWorker is activated.')
+  ev.waitUntil(async () => {
+    await ctx.registration.navigationPreload?.enable()
+  })
+})
+
+// To make the App installable, SW must have a 'fetch' event listener.
+ctx.addEventListener('fetch', () => {
+  /* noop */
 })
